ba5d7de38ebb14178975eff42a965f790c0002be
Ahsan Zia <mynameisahsanzia@gmail.com>
2026-05-05 13:16:10 +0500

    Favorite Controller added

--- backend/src/controllers/MovieController.ts
+++ backend/src/controllers/MovieController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import { MovieService } from "../services";
-// type IdParams = { id: string };
 
 class MovieController {
   async createMovie(req: Request, res: Response): Promise<void> {
